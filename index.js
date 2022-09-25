7c3a2cfef5bfc3583ea18eefe9d3655538e89ead
peterstaylor <31700136+peterstaylor@users.noreply.github.com>
2022-09-24 21:21:03 -0400

    switch to https

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const todayStats = "http://99.124.137.190:8181/api/v2?apikey=e226504f58444d879ce88803152b142a&cmd=get_home_stats&time_range=1&stats_type=duration&stats_count=3&stat_id=top_music"; 
+const todayStats = "https://99.124.137.190:8181/api/v2?apikey=e7feb684be924d9190e11ea18e0c8452&cmd=get_home_stats&time_range=1&stats_type=duration&stats_count=3&stat_id=top_music"; 
 
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
